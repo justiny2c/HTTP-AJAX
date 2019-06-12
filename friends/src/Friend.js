f9baae12bfface5fc96085d29b89c36dbf3a528c
justiny2c <justiny2c@gmail.com>
2019-06-12 15:18:15 -0700

    Delete handler working

--- friends/src/Friend.js
+++ friends/src/Friend.js
@@ -1,14 +1,27 @@
 import React from "react";
+import axios from "axios";
 
 class Friend extends React.Component {
     state = {
 
     }
 
+    deleteHandler = e => {
+
+        axios.delete(`http://localhost:5000/friends/${this.props.friend.id}`)
+        .then()
+    }
+
     render(){
         return (
             <div>
-                {this.props.friend.name}
+                <form>
+                    <h1>{this.props.friend.name}</h1>
+                    <button
+                        key = {this.props.friend.id}
+                        onClick = {this.deleteHandler}>X
+                    </button>
+                </form>
             </div>
         )
 
